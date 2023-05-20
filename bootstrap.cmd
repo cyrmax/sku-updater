958b9635078cf9b1f632ac705fd05eefa02834c4
Kirill Belousov <belousov.k.m@ya.ru>
2023-05-21 00:14:55 +0300

    First try to create github action for automatic releasing

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,2 +1,2 @@
 python -m pip install -r requirements.txt
-python -m pip install -r build-requirements-nuitka.txt
+python -m pip install -r build-requirements.txt
--- build.cmd
+++ build.cmd
@@ -1,1 +1,12 @@
-python -m nuitka --standalone --onefile --prefer-source-code --assume-yes-for-downloads --remove-output --lto=yes --windows-uac-admin --company-name=Cyrmax --product-name=Sku_Updater --file-version=32.15 --product-version=32.15 --file-description="A console program to update your installation of Sku for WoW Classic" --copyright="Made by Cyrmax in 2022. MIT license" sku-updater.py
+if "%CI%" == "true" (
+    set curver=%GITHUB_REF_NAME%
+) else (
+    if not "%1" == "" (
+        set curver=%1
+    ) else (
+        echo "Provide a valid version number"
+        exit
+    )
+)
+
+python -m nuitka --standalone --onefile --prefer-source-code --assume-yes-for-downloads --lto=yes --windows-uac-admin --company-name=Cyrmax --product-name=Sku_Updater --file-version=%curver% --product-version=%curver% --file-description="A console program to update your installation of Sku for WoW Classic" --copyright="Made by Cyrmax in 2022. MIT license" sku-updater.py
