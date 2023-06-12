aae17d0d3990b223252363a8f26d1dabf08401ae
Kirill Belousov <belousov.k.m@ya.ru>
2023-06-12 15:02:01 +0300

    Added remove output option to build script

--- build.cmd
+++ build.cmd
@@ -10,4 +10,4 @@ if "%CI%" == "true" (
 )
 
 python prepare_build_info.py
-python -m nuitka --standalone --onefile --prefer-source-code --assume-yes-for-downloads --lto=yes --company-name=Cyrmax --product-name=Sku_Updater --file-version=%curver% --product-version=%curver% --file-description="A console program to update your installation of Sku for WoW Classic" --copyright="Made by Cyrmax in 2022. MIT license" sku-updater.py
+python -m nuitka --standalone --onefile --prefer-source-code --assume-yes-for-downloads --remove-output --lto=yes --company-name=Cyrmax --product-name=Sku_Updater --file-version=%curver% --product-version=%curver% --file-description="A console program to update your installation of Sku for WoW Classic" --copyright="Made by Cyrmax in 2022. MIT license" sku-updater.py
