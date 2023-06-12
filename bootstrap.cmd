b2ac182bc6fb5fa7786a736f37899dfc11dfb0ee
Kirill Belousov <belousov.k.m@ya.ru>
2023-06-12 15:54:20 +0300

    Tried to force python3.11 while building.

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,2 +1,2 @@
-python -m pip install -r requirements.txt
-python -m pip install -r build-requirements.txt
+python3.11 -m pip install -r requirements.txt
+python3.11 -m pip install -r build-requirements.txt
