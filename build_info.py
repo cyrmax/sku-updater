c45c9998f9f027309edd045cda92d1af0a1ece76
Kirill Belousov <belousov.k.m@yandex.ru>
2023-09-07 23:01:08 +0300

    Upgraded Nuitka version to 1.8 for better results with Python 3.11. Still cannot fix virustotal false positives

--- build_info.py
+++ build_info.py
@@ -2,30 +2,28 @@
 # This file should be regenerated directly before build process for to contain actual info about build environment.
 sku_updater_version = "noversion"
 build_platform = "Windows-10-10.0.22621-SP0"
-build_python_version = "3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)]"
+build_python_version = "3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)]"
 pip_freeze_output = """
-beautifulsoup4==4.11.1
-black==23.3.0
-certifi==2022.12.7
-charset-normalizer==2.1.1
-click==8.1.3
+beautifulsoup4==4.12.2
+black==22.10.0
+certifi==2023.7.22
+charset-normalizer==3.2.0
+click==8.1.7
 colorama==0.4.6
 flake8==6.0.0
 idna==3.4
 mccabe==0.7.0
 mypy-extensions==1.0.0
-Nuitka==1.4.8
+Nuitka==1.8
 ordered-set==4.1.0
-packaging==23.1
-pathspec==0.11.1
-pip==22.2.2
-platformdirs==3.5.1
+pathspec==0.11.2
+pip==22.3.1
+platformdirs==3.10.0
 pycodestyle==2.10.0
 pyflakes==3.0.1
-requests==2.28.1
-setuptools==63.2.0
-soupsieve==2.4
-tomli==2.0.1
-urllib3==1.26.14
-zstandard==0.20.0
+requests==2.31.0
+setuptools==65.5.0
+soupsieve==2.5
+urllib3==2.0.4
+zstandard==0.21.0
 """
